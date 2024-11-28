276c1d0437f0fd792dae09f8165b5b092401eb7d
Francisco Soares <fmssn@cin.ufpe.br>
2024-11-28 10:44:23 -0300

    Initial Commit

---
+++ JogoExemplo/JogoExemploApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogoExemploApp.swift
+//  JogoExemplo
+//
+//  Created by Francisco Miranda Soares on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct JogoExemploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
